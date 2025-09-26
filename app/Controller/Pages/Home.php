523881c462f75eb682d1d5632e57bbcfd935df3e
BrunoCamiloRibeiro <brunocamiloribeirop@gmail.com>
2025-09-26 01:32:31 -0300

    primeiro commit

---
+++ app/Controller/Pages/Home.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller\Pages;
+
+class Home
+{    
+    /**
+     * Método responsavel por retornar o conteúdo (view) da Home.
+     *
+     * @return string
+     */
+    public static function getHome()
+    {
+
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'bruno/ihc',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'bruno/ihc' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
